b3a963b258a30f173701aa6b7a9479b0f82f5e26
NeoAxiøm <shouryasrivastava2011@gmail.com>
2026-07-07 22:33:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ document.addEventListener("pointermove",e=>{
   document.documentElement.style.setProperty("--x",e.clientX+"px");
   document.documentElement.style.setProperty("--y",e.clientY+"px");
 });
+alert("JS LOADED!");
